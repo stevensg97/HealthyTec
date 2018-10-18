da68951954cb5fd67f58020544ea0f0866ce73aa
Steven <maiderr97@gmail.com>
2018-10-18 15:58:56 -0600

    [Add] Proyecto
    Creación del proyecto en git

---
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AdminLayoutRoutes } from './admin-layout.routing';
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { UserProfileComponent } from '../../user-profile/user-profile.component';
+import { TableListComponent } from '../../table-list/table-list.component';
+import { TypographyComponent } from '../../typography/typography.component';
+import { IconsComponent } from '../../icons/icons.component';
+import { NotificationsComponent } from '../../notifications/notifications.component';
+import { CentroComponent } from '../../centro/centro.component';
+import { FuncionarioComponent } from '../../funcionario/funcionario.component';
+import { DiagnosticosComponent } from '../../diagnosticos/diagnosticos.component';
+import { TratamientosComponent } from '../../tratamientos/tratamientos.component';
+import { AlgoComponent } from '../../algo/algo.component';
+
+import {
+  MatButtonModule,
+  MatInputModule,
+  MatRippleModule,
+  MatTooltipModule,
+} from '@angular/material';
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(AdminLayoutRoutes),
+    FormsModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatInputModule,
+    MatTooltipModule,
+  ],
+  declarations: [
+    DashboardComponent,
+    UserProfileComponent,
+    TableListComponent,
+    TypographyComponent,
+    IconsComponent,
+    NotificationsComponent,
+    CentroComponent,
+    FuncionarioComponent,
+    DiagnosticosComponent,
+    TratamientosComponent,
+    AlgoComponent,
+  ]
+})
+
+export class AdminLayoutModule {}
